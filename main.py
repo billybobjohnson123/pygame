25c4679250948d73feb9f83e26d465dba540ea72
billybobjohnson123 <lif83866@gmail.com>
2026-06-09 19:03:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# Example file showing a basic pygame "game loop"
+# Example file showing a circle moving on screen
 import pygame
+<<<<<<< Updated upstream
 from liquid import Liquid
 from block import Block
 
@@ -26,6 +27,17 @@ scale = 100
 objects = []
 backround.fill((0,0,0))
 blocks = []
+=======
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+>>>>>>> Stashed changes
 
 while running:
     screen.blit(backround, (0,0))
@@ -37,6 +49,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+<<<<<<< Updated upstream
         if event.type == pygame.MOUSEBUTTONUP and event.button == 1: 
             x, y = event.pos
             pickedup = False
@@ -67,4 +80,30 @@ while running:
     # flip() the display to put your work on screen
     pygame.display.flip()
     clock.tick(60)  # limits FPS to 60
+=======
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+>>>>>>> Stashed changes
 pygame.quit()
